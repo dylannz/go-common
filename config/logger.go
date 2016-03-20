0d059c1fe386effac9f66c462bdbd5de0f1ade14
Nic Day <nic.day@me.com>
2016-03-21 10:45:20 +1300

    Log log level after set

--- config/logger.go
+++ config/logger.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/HomesNZ/go-common/env"
@@ -12,13 +11,11 @@ import (
 func InitLogger() {
 	logrus.SetOutput(os.Stdout)
 
-	logrus.Infof("Log level: %s", logrus.DebugLevel)
-
 	level, err := logrus.ParseLevel(env.GetString("LOG_LEVEL", "info"))
 	// No need to handle the error here, just don't update the log level
 	if err == nil {
-		logrus.Info("Setting log level to ", level.String())
-		fmt.Println("")
 		logrus.SetLevel(level)
 	}
+
+	logrus.Infof("Log level: %s", logrus.GetLevel().String())
 }
