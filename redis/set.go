f1da4d2ce0787cef00109939a97e00b5e3652b07
Dylan Macdonald <dylan.macdonald@homes.co.nz>
2016-07-19 13:54:11 +1200

    Add a new setEx method with support for golang times

--- redis/set.go
+++ redis/set.go
@@ -1,5 +1,9 @@
 package redis
 
+import (
+	"time"
+)
+
 // Set adds a new key value pair to the redis cache.
 func (c Cache) Set(key, val string) error {
 	conn := c.Conn()
@@ -19,6 +23,19 @@ func (c Cache) SetExpiry(key, val string, expireTime int) error {
 	defer conn.Close()
 
 	_, err := conn.Do("SETEX", key, expireTime, val)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c Cache) SetExpiryTime (key,val string, expireTime time.Time) error{
+	conn := c.Conn()
+	defer conn.Close()
+	expire := expireTime.Unix() - time.Now().Unix()
+
+	_, err := conn.Do("SETEX", key, int(expire), val)
 	if err != nil {
 		return err
 	}
