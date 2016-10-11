39ea95e26098ff7eac97689313084d87cd94bb2d
Dylan Jennings <dylan.jennings@homes.co.nz>
2016-10-12 10:58:48 +1300

    Use smartystreets/go-aws-auth to sign requests

--- elasticsearch/connection.go
+++ elasticsearch/connection.go
@@ -1,12 +1,14 @@
 package elasticsearch
 
 import (
+	"net/http"
 	"strings"
 	"sync"
 
 	"github.com/HomesNZ/go-common/env"
 
 	"github.com/HomesNZ/elastic"
+	"github.com/smartystreets/go-aws-auth"
 )
 
 var (
@@ -14,15 +16,23 @@ var (
 	initOnce = sync.Once{}
 )
 
+func awsAuth() bool {
+	key := env.GetString("AWS_ACCESS_KEY_ID", "")
+	secret := env.GetString("AWS_SECRET_ACCESS_KEY", "")
+	token := env.GetString("AWS_SECURITY_TOKEN", "")
+	return key != "" && secret != "" || token != ""
+}
+
 func initConn() {
 	// Create a client
 	var err error
 	options := []elastic.ClientOptionFunc{
 		elastic.SetURL(strings.Split(env.MustGetString("ELASTICSEARCH_URLS"), ";")...),
 	}
-	authorization := env.GetString("ELASTICSEARCH_IAM_AUTHORIZATION", "")
-	if authorization != "" {
-		options = append(options, elastic.SetAuthorizationHeader(authorization))
+	if awsAuth() {
+		options = append(options, elastic.SetPrepareRequest(func(req *http.Request) {
+			awsauth.Sign(req)
+		}))
 	}
 	conn, err = elastic.NewClient(options...)
 	if err != nil {
