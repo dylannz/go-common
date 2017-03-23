4aa4a552a9d3356333fda11f73f73a772604c35f
Dylan Jennings <dylan.jennings@homes.co.nz>
2017-03-24 11:32:19 +1300

    Add newrelic package

---
+++ newrelic/newrelic.go
@@ -0,0 +1,29 @@
+package newrelic
+
+import (
+	"sync"
+
+	"github.com/HomesNZ/go-common/env"
+	newrelic "github.com/newrelic/go-agent"
+)
+
+var (
+	app      newrelic.Application
+	initOnce = sync.Once{}
+)
+
+// InitNewRelic initializes the NewRelic configuration and panics if there is an
+// error.
+func InitNewRelic(appName string) {
+	var err error
+	config := newrelic.NewConfig(appName, env.MustGetString("NEWRELIC_API_KEY"))
+	app, err = newrelic.NewApplication(config)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// App returns the NewRelic application
+func App() newrelic.Application {
+	return app
+}
