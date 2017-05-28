a1cbb5550a70e0f324d504505732ccfedd77066a
Dylan Macdonald <dylan.macdonald@homes.co.nz>
2017-05-29 11:11:35 +1200

    Refactor, remove tests

--- email/emailer.go
+++ email/emailer.go
@@ -1,26 +1,8 @@
-// Email contains a basic implementation of a SMTP email sender.  Usage :
-//			em := email.NewEmailSender(email.EmailConfig{
-//				ServerHostPort: "email-smtp.us-west-2.amazonaws.com:587",
-//				Username: "AKIAI4CUKIYPFY6QB6IQ",
-//				Password:"AiklA0mqVJnfJzctVHUImRduU5/8pqkUsEalj8Giz7Qe",
-//			})
-//
-//			err := em.SendMail(&email.Email{
-//				To:      "data@homes.co.nz",
-//				From:    "greg.day@homes.co.nz",
-//				Subject: "Test email",
-//				Body:    "Test body",
-//			})
-
 package email
 
 import (
-	"bytes"
-	"fmt"
-	"net"
 	"net/smtp"
 	"sync"
-	"time"
 
 	"github.com/HomesNZ/go-common/env"
 	"github.com/Sirupsen/logrus"
@@ -30,7 +12,7 @@ import (
 var (
 	contextLogger = logrus.WithField("email", "sender")
 	once          sync.Once
-	Emailer       Interface
+	Emailer       EmailSender
 )
 
 type EmailConfig struct {
@@ -41,72 +23,37 @@ type EmailConfig struct {
 
 type EmailSender struct {
 	Conf EmailConfig
-	Send func(string, smtp.Auth, string, []string, []byte) error
 }
 
 func InitEmailer() {
-	Emailer = NewEmailSender(EmailConfig{
-		ServerHostPort: env.MustGetString("SMTP_HOST"),
-		Username:       env.MustGetString("SMTP_USER"),
-		Password:       env.MustGetString("SMTP_PASSWORD"),
-	})
-}
-
-func NewEmailSender(conf EmailConfig) Interface {
-	return &EmailSender{conf, smtp.SendMail}
-}
-
-// email.Interface is an interface to a struct that 'sends' emails. It allows the send function to be mocked out for testing.
-type Interface interface {
-	SendMail(email *Email) error
-	SendMailWithAttachment(email *Email) error
-}
-
-func (e *EmailSender) createEmailBody(email *Email) (smtp.Auth, *bytes.Buffer) {
-	host, _, _ := net.SplitHostPort(e.Conf.ServerHostPort)
-	auth := smtp.PlainAuth("", e.Conf.Username, e.Conf.Password, host)
-
-	headers := make(map[string]string)
-	headers["From"] = email.From
-	headers["To"] = email.To
-	headers["Subject"] = email.Subject
-	headers["Date"] = time.Now().Format(time.UnixDate)
-
-	message := bytes.NewBufferString("")
-	for k, v := range headers {
-		message.WriteString(fmt.Sprintf("%s: %s\r\n", k, v))
+	Emailer = EmailSender{
+		Conf: EmailConfig{
+			ServerHostPort: env.MustGetString("SMTP_HOST"),
+			Username:       env.MustGetString("SMTP_USER"),
+			Password:       env.MustGetString("SMTP_PASSWORD"),
+		},
 	}
-
-	message.WriteString("\r\n")
-	message.WriteString(email.Body)
-	return auth, message
 }
 
 // Send sends a simple email via a smtp gateway using TLS
-func (e *EmailSender) SendMail(email *Email) error {
+func Send(content *Email) error {
 	once.Do(func() {
 		InitEmailer()
 	})
-	auth, message := e.createEmailBody(email)
-	return e.Send(e.Conf.ServerHostPort, auth, email.From, []string{email.To}, message.Bytes())
-}
+	auth := smtp.PlainAuth("", Emailer.Conf.Username, Emailer.Conf.Password, Emailer.Conf.ServerHostPort)
 
-// SendMailWithAttachment sends an email with an Attachment using the content defined by Email
-func (e *EmailSender) SendMailWithAttachment(emailContent *Email) error {
-	once.Do(func() {
-		InitEmailer()
-	})
-	auth, body := e.createEmailBody(emailContent)
 	eml := &email.Email{
-		To:      []string{emailContent.To},
-		From:    emailContent.From,
-		Subject: emailContent.Body,
-		Text:    body.Bytes(),
+		To:      []string{content.To},
+		From:    content.From,
+		Subject: content.Subject,
+		Text:    []byte(content.Body),
 	}
 
-	_, err := eml.AttachFile(emailContent.Attachment)
-	if err != nil {
-		return err
+	if content.Attachment != "" {
+		_, err := eml.AttachFile(content.Attachment)
+		if err != nil {
+			return err
+		}
 	}
-	return eml.Send(e.Conf.ServerHostPort, auth)
+	return eml.Send(Emailer.Conf.ServerHostPort, auth)
 }
