0504a5d0765fa12c8b2dc93973ccc081b4e849a9
Dylan Jennings <dylan.jennings@homes.co.nz>
2017-09-14 11:13:12 +1200

    Revert "Update nzAddress to handle hyphentated streetnumbers"
    This reverts commit 9b7894318294ab72028e54f20057b69260f04a4a.

--- nz_address/address.go
+++ nz_address/address.go
@@ -10,7 +10,7 @@ import (
 type Address struct {
 	UnitType        string `json:"unit_type"`
 	UnitIdentifier  string `json:"unit_identifier"`
-	StreetNumber    string `json:"street_number"`
+	StreetNumber    int    `json:"street_number"`
 	StreetAlpha     string `json:"street_alpha"`
 	StreetName      string `json:"street_name"`
 	StreetType      string `json:"street_type"`
@@ -57,14 +57,14 @@ func (a Address) Display() string {
 
 	var identifierStreet string
 	street := titleCase(a.Street())
-	if a.StreetNumber != "" {
+	if a.StreetNumber != 0 {
 		if a.UnitIdentifier != "" && a.BuildingName == "" {
 			if a.UnitType != "" {
 				identifierStreet += titleCase(a.UnitType) + " "
 			}
 			identifierStreet += strings.ToUpper(a.UnitIdentifier) + "/"
 		}
-		identifierStreet += a.StreetNumber + strings.ToUpper(a.StreetAlpha)
+		identifierStreet += strconv.Itoa(a.StreetNumber) + strings.ToUpper(a.StreetAlpha)
 		if street != "" {
 			identifierStreet += " "
 		}
